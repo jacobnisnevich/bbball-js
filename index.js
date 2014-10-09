d1764bcd5e9896c4dbcfb7118188a635abb4dc51
jacobnisnevich <jacob.nisnevich@gmail.com>
2014-10-09 11:54:08 -0700

    server-side pixi.js

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 var express = require('express')
 var app = express();
-var cool = require('cool-ascii-faces');
+//var cool = require('cool-ascii-faces');
+var pixi = require('pixi');
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-  response.send(cool());
-});
+//app.get('/', function(request, response) {
+//  response.send(cool());
+//});
+
+var renderer = pixi.WebGLRenderer(800, 600);
+document.body.appendChild(renderer.view);
+var stage = new pixi.Stage();
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
