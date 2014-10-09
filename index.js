35c7c5d4fa325b21297101625a106fed1c904b00
jacobnisnevich <jacob.nisnevich@gmail.com>
2014-10-09 14:17:22 -0700

    removed pixi from server-side

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 var express = require('express')
 var app = express();
-//var cool = require('cool-ascii-faces');
-var pixi = require('pixi');
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-//app.get('/', function(request, response) {
-//  response.send(cool());
-//});
 
 var renderer = pixi.WebGLRenderer(800, 600);
 document.body.appendChild(renderer.view);
