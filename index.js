3d76a98d2483371a241109fc791f5cc274fdac49
jacobnisnevich <jacob.nisnevich@gmail.com>
2014-10-09 14:19:24 -0700

    properly removed pixi

--- index.js
+++ index.js
@@ -4,11 +4,6 @@ var app = express();
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-
-var renderer = pixi.WebGLRenderer(800, 600);
-document.body.appendChild(renderer.view);
-var stage = new pixi.Stage();
-
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
