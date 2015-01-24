e5841bbacca9d915273386ddc34506d26a56db6b
jacobnisnevich <jacob.nisnevich@gmail.com>
2015-01-23 20:32:09 -0800

    initial html commit

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-var express = require('express')
+var request = require('request');
+var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.set('view engine', 'html');
+app.enable('view cache');
+app.engine('html', require('hogan-express'));
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+app.use("/styles", express.static(__dirname + '/styles'));
+app.use("/js", express.static(__dirname + '/js'));
+app.use("/images", express.static(__dirname + '/images'));
+app.use(bodyParser());
+
+app.get('/', function(req, res) {
+	res.render('index.html')
+})
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+	console.log("Node app is running at localhost:" + app.get('port'))
 })
